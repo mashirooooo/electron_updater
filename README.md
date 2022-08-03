6042879918b1d6f22a9a06ac7e2d956379311354
mashirooooo <chenandmashiro@gmail.com>
2022-08-04 01:17:23 +0800

    修改结束electron进程方式

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # electron_updater
+
 electron 增量更新仓库
+
+* electron应该在可以安全退出的情况下调用更新程序，并在调用后退出electron程序，防止更新出错；
+* 更新程序会尝试结束electron程序，如果没有结束掉electron程序将不会继续运行；
+* 如果更新程序没有安装，则它将被下载到用户的临时文件夹中，并在更新完成后被删除；todo
--- src/sysinfo.rs
+++ src/sysinfo.rs
@@ -1,6 +1,8 @@
 use std::path::Path;
 use sysinfo::{ProcessExt, SystemExt};
 
+use crate::mlog::{Log, Logtrait};
+
 /// 结束electron的进程
 ///
 /// # Examples
@@ -11,13 +13,40 @@ use sysinfo::{ProcessExt, SystemExt};
 ///
 /// ```
 pub fn end_electron_main<P: AsRef<Path>>(path: P) -> bool {
-    let sys = sysinfo::System::new_all();
+    let mut sys = sysinfo::System::new_all();
     let current_exe_path = std::env::current_exe().unwrap();
     //注意 如果有其他进程的执行exe的路径是直接kill掉处理
+    sys.processes().iter().for_each(|(_pid, process)| {
+        match process.exe() {
+            v if v.starts_with(path.as_ref())
+                && v != current_exe_path
+                && process.cmd().iter().any(|x| x == "/prefetch:1") =>
+            {
+                // Log::info(format!("{process:#?}").as_str());
+                process.kill();
+            }
+            _ => (),
+        }
+    });
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    sys.refresh_all();
     sys.processes()
         .iter()
-        .all(|(_pid, process)| match process.exe() {
-            v if v.starts_with(path.as_ref()) && v != current_exe_path => process.kill(),
-            _ => true,
-        })
+        .for_each(|(_pid, process)| match process.exe() {
+            v if v.starts_with(path.as_ref()) && v != current_exe_path => {
+                process.kill();
+            }
+            _ => (),
+        });
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    sys.refresh_all();
+    sys.processes().iter().any(|(_pid, process)| {
+        let v = process.exe();
+        let r = v.starts_with(path.as_ref()) && v != current_exe_path;
+        if r {
+            Log::error(format!("存在未退出的electron进程: {process:#?}").as_str());
+            panic!("存在未退出的electron进程{:?}", process.exe());
+        }
+        r
+    })
 }
